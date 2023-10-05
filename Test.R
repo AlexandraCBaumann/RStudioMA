a4feb5b883ef3f47073731fa6e22b414c09e0893
AlexandraCBaumann <alexandra.c.baumann@gmx.de>
2023-10-05 12:03:25 +0200

    Trajectory loop + data frames for each individual

--- Test.R
+++ Test.R
@@ -11,8 +11,7 @@ library(recurse)
 library(ggplot2)
 library(patchwork)
 library(sp)
-#library(lubridate)
-#install.packages("lubridate")
+
 
 # Set your working directory
 setwd("D:/Desktop/Masterarbeit/GitRepository/RStudioMA")
@@ -20,106 +19,160 @@ setwd("D:/Desktop/Masterarbeit/GitRepository/RStudioMA")
 # Read the CSV file
 allfemales_data <- read.csv("clipped_allfemales_till02-2023_UTM_unixtime.csv")
 
-# List of IDs you want to split by
-ids_to_split <- c(187026, 187369, 187372, 287370, 87026, 87368, 87369, 87371, 87371, 87372, 87373, 87373, 87374, 87374, 87375, 87375, 87376, 87376, 87377, 87378, 87379, 87380, 87380, 87381, 87381, 87382, 87382, 87383, 87383, 87384, 87384, 87385, 87385, 87386, 87386, 87388, 87389, 87390, 87391, 87391, 87393, 87395, 87396, 87397, 89183, 89184, 89185, 89186, 89187, 89188, 89189, 89190, 89191, 89192, 89193, 187370, 187377, 187388, 89191, 89192, 89193, 89188, 89189, 89183, 89184, 87393, 87395, 87396, 87389, 87390, 187382, 189186)
+# Clean and standardize the IDs in the 'ID' column
+allfemales_data$ID <- as.character(allfemales_data$ID)  # Convert to character
+allfemales_data$ID <- gsub("\"", "", allfemales_data$ID)  # Remove double quotes if present
+allfemales_data$ID <- gsub("^0+", "", allfemales_data$ID)  # Remove leading zeros
 
-# Create a list of data frames, each containing data for a specific individual
-individual_data_frames <- split(allfemales_data, allfemales_data$ID)
+# Get unique, cleaned IDs
+unique_ids <- unique(allfemales_data$ID)
 
-# Loop through each ID in ids_to_split
-for (i in ids_to_split) {
-  # Check if the ID exists in the list
-  if (i %in% names(individual_data_frames)) {
-    # Get the data frame for the current individual
-    current_data_frame <- individual_data_frames[[i]]
-    
-    # Rename columns "UTM_Northing" to "x", "UTM_Easting" to "y", and "Unix_Time" to "time"
-    colnames(current_data_frame)[colnames(current_data_frame) == "UTM_Northing"] <- "x"
-    colnames(current_data_frame)[colnames(current_data_frame) == "UTM_Easting"] <- "y"
-    colnames(current_data_frame)[colnames(current_data_frame) == "Unix_Time"] <- "time"
-    
-    # Replace the data frame in the list with the updated data frame
-    individual_data_frames[[i]] <- current_data_frame
-  }
+# Create an empty list to store data frames
+individual_data_frames <- list()
+
+
+# Loop through unique IDs and split data
+for (id in unique_ids) {
+  # Subset data for the current ID
+  current_data <- allfemales_data[allfemales_data$ID == id, ]
+
+  # Rename columns if needed (similar to your previous code)
+  colnames(current_data)[colnames(current_data) == "UTM_Northing"] <- "x"
+  colnames(current_data)[colnames(current_data) == "UTM_Easting"] <- "y"
+  colnames(current_data)[colnames(current_data) == "Unix_Time"] <- "time"
+
+
+  # Store the data frame in the list
+  individual_data_frames[[id]] <- current_data
 }
-
-#define x, y and time
-colnames(data)[colnames(allfemales_data) == "UTM_Northing"] <- "x"
-colnames(data)[colnames(allfemales_data) == "UTM_Easting"] <- "y"
-colnames(data)[colnames(allfemales_data) == "Unix_Time"] <- "time"
-
-
-# Create a list to store individual plots
-individual_plots <- list()
 
 #####FILTER TRAJECTORIES ####
 
+# Loop through the list of individual data frames to calculate speed and angle
+for (id in unique_ids) {
+  # Get the current individual's data frame
+  current_data <- individual_data_frames[[id]]
+  # Convert the data frame to a data.table
+  current_data <- as.data.table(current_data)
+  #Calculate incoming and outgoing speed columns
+  current_data[, `:=`(
+    speed_in = atl_get_speed(current_data,
+                             x = "x",
+                             y = "y",
+                             time = "time"),
+    speed_out = atl_get_speed(current_data, type = "out")
+  )]
+
+  
+  # Calculate turning angle column
+  current_data[, angle := atl_turning_angle(data = current_data)]
+  
+  # Calculate speed and angle thresholds
+  speed_angle_thresholds <-
+    sapply(current_data[, list(speed_in, speed_out, angle)],
+           quantile,
+           probs = 0.9, na.rm = TRUE
+    )
+  
+  # Store the updated data frame in the list
+  individual_data_frames[[id]] <- current_data
+}
+
+# Create an empty list to store individual plots
+individual_plots <- list()
+
+# Loop through unique IDs and create ggplot2 plots
+for (id in unique_ids) {
+  # Get the current individual's data frame
+  current_data <- individual_data_frames[[id]]
+  
+  # Print the ID to check which one is being processed
+  print(id)
+  
+  # Create a ggplot2 plot for the current individual
+  individual_plots[[id]] <- ggplot(current_data, aes(x = x, y = y, group = ID, color = as.factor(ID))) +
+    geom_path() +
+    labs(title = paste("Individual ID:", id),
+         x = "X Coordinate",
+         y = "Y Coordinate") +
+    theme_minimal()
+}
+
+# Print the individual plots to the console
+print(individual_plots)
 
 
 
-# add incoming and outgoing speed
-allfemales_data[, `:=`(
-  speed_in = atl_get_speed(allfemales_data,
-                           x = "x",
-                           y = "y",
-                           time = "time"
-  ),
-  speed_out = atl_get_speed(allfemales_data, type = "out")
-)]
+# # Loop through the list of individual data frames to process each one
+# for (id in unique_ids) {
+#   # Get the current individual's data frame
+#   current_data <- individual_data_frames[[id]]
+#   
+#   # Remove speed outliers
+#   current_data <- atl_filter_covariates(
+#     allfemales_data = current_data,
+#     filters = c("(speed_in < 15 & speed_out < 15)")
+#   )
+#   
+#   # Recalculate speed and angle
+#   current_data[, `:=`(
+#     speed_in = atl_get_speed(current_data, x = "x", y = "y", time = "time"),
+#     speed_out = atl_get_speed(current_data, type = "out")
+#   )]
+#   
+#   current_data[, angle := atl_turning_angle(allfemales_data = current_data)]
+#   
+#   # Apply the atl_median_smooth function
+#   current_data <- atl_median_smooth(
+#     allfemales_data = current_data,
+#     x = "x", y = "y", time = "time",
+#     moving_window = 5
+#   )
+#   
+#   # Update the data frame in the list
+#   individual_data_frames[[id]] <- current_data
+# }
 
-# add turning angle
-allfemales_data[, angle := atl_turning_angle(allfemales_data = allfemales_data)]
-
-# use sapply
-speed_angle_thresholds <-
-  sapply(allfemales_data[, list(speed_in, speed_out, angle)],
-         quantile,
-         probs = 0.9, na.rm = T
-  )
-
-
-# remove speed outliers
-allfemales_data <- atl_filter_covariates(
-  allfemales_data = allfemales_data,
-  filters = c("(speed_in < 15 & speed_out < 15)")
-)
-
-# recalculate speed and angle
-allfemales_data[, `:=`(
-  speed_in = atl_get_speed(allfemales_data,
-                           x = "x",
-                           y = "y",
-                           time = "time"
-  ),
-  speed_out = atl_get_speed(allfemales_data, type = "out")
-)]
-
-# add turning angle
-allfemales_data[, angle := atl_turning_angle(allfemales_data = allfemales_data)]
-
-
-
-# now apply the smooth
-atl_median_smooth(
-  allfemales_data = allfemales_data,
-  x = "x", y = "y", time = "time",
-  moving_window = 5
-)
+# 
+# 
+# # remove speed outliers
+# allfemales_data <- atl_filter_covariates(
+#   allfemales_data = allfemales_data,
+#   filters = c("(speed_in < 15 & speed_out < 15)")
+# )
+# 
+# # recalculate speed and angle
+# allfemales_data[, `:=`(
+#   speed_in = atl_get_speed(allfemales_data,
+#                            x = "x",
+#                            y = "y",
+#                            time = "time"
+#   ),
+#   speed_out = atl_get_speed(allfemales_data, type = "out")
+# )]
+# 
+# # add turning angle
+# allfemales_data[, angle := atl_turning_angle(allfemales_data = allfemales_data)]
+# 
+# 
+# 
+# # now apply the smooth
+# atl_median_smooth(
+#   allfemales_data = allfemales_data,
+#   x = "x", y = "y", time = "time",
+#   moving_window = 5
+# )
 
 
 # Define the UTM32N CRS
 utm_crs <- st_crs("+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs")
 
-# List of IDs to plot
-ids_to_plot <- c(187026, 187369, 187372, 287370, 087026, 087368, 087369, 087371, 087371, 087372, 087373, 087373, 087374, 087374, 087375, 087375, 087376, 087376, 087377, 087378, 087379, 087380, 087380, 087381, 087381, 087382, 087382, 087383, 087383, 087384, 087384, 087385, 087385, 087386, 087386, 087388, 087389, 087390, 087391, 087391, 087393, 087395, 087396, 087397, 089183, 089184, 089185, 089186, 089187, 089188, 089189, 089190, 089191, 089192, 089193, 187370, 187377, 187388, 089191, 089192, 089193, 089188, 089189, 089183, 089184, 087393, 087395, 087396, 087389, 087390, 187382, 189186)
 
-# Filter data for selected IDs
-filtered_data <- data[data$ID %in% ids_to_plot, ]
-
-# Create a ggplot object to plot trajectories
-ggplot(filtered_data, aes(x = x, y = y, group = ID, color = as.factor(ID))) +
-  geom_path() +
-  labs(title = "Trajectories of Individuals",
-       x = "X Coordinate",
-       y = "Y Coordinate") +
-  theme_minimal()
+# 
+# # Create a ggplot object to plot trajectories
+# ggplot(filtered_data, aes(x = x, y = y, group = ID, color = as.factor(ID))) +
+#   geom_path() +
+#   labs(title = "Trajectories of Individuals",
+#        x = "X Coordinate",
+#        y = "Y Coordinate") +
+#   theme_minimal()
